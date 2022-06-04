8042abdaf601725625a81afda3c700865876096c
alexntwu <alexnt.wu@gmail.com>
2022-06-04 22:41:20 +0800

    first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+		// argv: array of references to String object
+		int i, sum;
+		for (i = 1, sum = 0; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println("summation from 1 to 10 is: "+sum);
+	}
+	// add f1 to dev1
+
+	// add f2 to dev2
+	
+}
